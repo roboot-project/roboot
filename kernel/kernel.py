b831fa29178f316a9c403a2bd1284f327613f79b
RevelMind Games <36819949+RevelMind@users.noreply.github.com>
2018-07-22 16:44:59 -0700

    Update kernel.py

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,1 +1,8 @@
 """intialize booter"""
+import jpype 
+jpype.startJVM(path to jvm.dll, "-ea") 
+javaPackage = jpype.JPackage("boot") 
+javaClass = javaPackage.JavaClassName 
+javaObject = javaClass() 
+javaObject.JavaMethodName() 
+jpype.shutdownJVM() 
